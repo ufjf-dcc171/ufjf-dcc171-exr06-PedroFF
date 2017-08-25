342bd8496bf56f278c051b9da8e67a81ed5b067e
Pedro Freitas <ice@lab06m28.nrc.ice.ufjf.br>
2017-08-24 22:47:49 -0300

    Exercicio 6

--- src/aula05exercicio2/JanelaPedraPapelTesoura.java
+++ src/aula05exercicio2/JanelaPedraPapelTesoura.java
@@ -1,4 +1,3 @@
-
 package aula05exercicio2;
 
 import java.awt.FlowLayout;
@@ -19,9 +18,11 @@ Acrescente um botão para realizar o cálculo.
 A jogada da “máquina” de ser realizada pelo java.util.Random (As chances não precisam ser simétricas).
 Exiba um placar final após 7 rodada
  */
-public class JanelaPedraPapelTesoura extends JFrame{
+public class JanelaPedraPapelTesoura extends JFrame {
+
     private final JLabel msgm = new JLabel("BEM VINDO AO PEDRA, PAPEL E TESOURA");
-    private int contaRodadas = 0;
+    private final JLabel msgm2 = new JLabel("");
+    private int contaRodadas = 1;
     private int vitoriaP1 = 0;
     private int vitoriaP2 = 0;
     private JRadioButton pedra = new JRadioButton("Pedra");
@@ -29,10 +30,10 @@ public class JanelaPedraPapelTesoura extends JFrame{
     private JRadioButton tesoura = new JRadioButton("Tesoura");
     private final ButtonGroup jogoGrupo;
     private int valorEscolhido;
-    
+
     public JanelaPedraPapelTesoura() throws HeadlessException {
         super("Pedra, papel e tesoura.");
-        int [] valores ={1,2,3};
+        int[] valores = {1, 2, 3};
         Random random = new Random();
         valorEscolhido = random.nextInt(3);
         setLayout(new FlowLayout());
@@ -40,42 +41,50 @@ public class JanelaPedraPapelTesoura extends JFrame{
         add(pedra);
         add(papel);
         add(tesoura);
-        
+        add(msgm2);
         jogoGrupo = new ButtonGroup();
         jogoGrupo.add(pedra);
         jogoGrupo.add(papel);
         jogoGrupo.add(tesoura);
-        
+
         int iPapel = 1;
         int iTesoura = 2;
         int iPedra = 3;
-        
+
         pedra.addItemListener(new Jogo(iPedra));
         papel.addItemListener(new Jogo(iPapel));
         tesoura.addItemListener(new Jogo(iTesoura));
     }
 
     private class Jogo implements ItemListener {
+
         int i;
+
         private Jogo(int i) {
             this.i = i;
         }
 
         @Override
         public void itemStateChanged(ItemEvent e) {
-            if(i == 3 && valorEscolhido == 1){
-                vitoriaP1++;
-                contaRodadas++;
-            }else if(i>valorEscolhido){
-                vitoriaP1++;
-                contaRodadas++;
-            }else if(i == valorEscolhido){
-                contaRodadas++;
-            }else{
-                vitoriaP2++;
-                contaRodadas++;
+            while (contaRodadas < 8) {
+                if (i == 1 && valorEscolhido == 3) {
+                    vitoriaP1++;
+                    contaRodadas++;
+                    msgm2.setText("Jogador: " + " Computador: " + " Rodadas: " + contaRodadas);
+                } else if (i > valorEscolhido) {
+                    vitoriaP1++;
+                    contaRodadas++;
+                    msgm2.setText("Jogador: " + " Computador: " + " Rodadas: " + contaRodadas);
+                } else if (i == valorEscolhido) {
+                    contaRodadas++;
+                    msgm2.setText("Jogador: " + " Computador: " + " Rodadas: " + contaRodadas);
+                } else {
+                    vitoriaP2++;
+                    contaRodadas++;
+                    msgm2.setText("Jogador: " + " Computador: " + " Rodadas: " + contaRodadas);
+                }
             }
         }
     }
-    
+
 }
